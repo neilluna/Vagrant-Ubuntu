3a16c12d010333a918e343fe279175c0bbe7b8c1
Neil Luna <nluna@paraselene.com>
2018-08-01 11:35:40 -0500

    Removed tabs and trailing whitespace characters, and fixed indentation

--- bin.vm-only/set-permissions.sh
+++ bin.vm-only/set-permissions.sh
@@ -18,45 +18,45 @@ set -u # Error out on the use of any undefined variables.
 # This script should only be run on the VM. It is called from both
 # 'provision-vagrant.sh' and 'sync-vm.sh' on the VM's project directory.
 
-echo_usage() { 
-	echo "This script will set the file permissions on all files and"
-	echo "directories in the project directory tree."
-	echo
-	echo "Usage: $(basename ${0}) [-h]"
-	echo
-	echo "   -h Show this help information."
-} 
+echo_usage() {
+  echo "This script will set the file permissions on all files and"
+  echo "directories in the project directory tree."
+  echo
+  echo "Usage: $(basename ${0}) [-h]"
+  echo
+  echo "  -h Show this help information."
+}
 
 while getopts ":h" opt; do
-	case "${opt}" in
-		h)
-			echo_usage ${0}
-			exit 0
-			;;
-		\?)
-			echo "${0}: Error: Invalid option: -${OPTARG}" >&2
-			echo_usage ${0}
-			exit 1
-			;;
-	esac
+  case "${opt}" in
+    h)
+      echo_usage ${0}
+      exit 0
+      ;;
+    \?)
+      echo "${0}: Error: Invalid option: -${OPTARG}" >&2
+      echo_usage ${0}
+      exit 1
+      ;;
+  esac
 done
 
 # Apply the excludes to the file list on stdin.
 # chmod the files in the final list.
 # Parameter is a chmod mode to apply to the files in the final list.
-exclude_and_chmod() { 
-	rm -f ${temp_dir}/names
-	while read -r data; do
-		echo "${data}" >> ${temp_dir}/names
-	done
-	while read -r line; do
-		grep -Pv ${line} ${temp_dir}/names > ${temp_dir}/names.new
-		mv -f ${temp_dir}/names.new ${temp_dir}/names
-	done < ${temp_dir}/exclude
-	while read -r line; do
-		chmod ${1} "${line}"
-	done < ${temp_dir}/names
-} 
+exclude_and_chmod() {
+  rm -f ${temp_dir}/names
+  while read -r data; do
+    echo "${data}" >> ${temp_dir}/names
+  done
+  while read -r line; do
+    grep -Pv ${line} ${temp_dir}/names > ${temp_dir}/names.new
+    mv -f ${temp_dir}/names.new ${temp_dir}/names
+  done < ${temp_dir}/exclude
+  while read -r line; do
+    chmod ${1} "${line}"
+  done < ${temp_dir}/names
+}
 
 # Change to the Vagrant synced_folder directory.
 cd /vagrant
@@ -67,10 +67,10 @@ temp_dir=$(mktemp -dt "$(basename ${0}).XXXXXXXXXX")
 egrep -v '^[[:space:]]*(#.*)?$' bin.vm-only/set-permissions-exclude > ${temp_dir}/exclude
 
 # Permission changers.
-find . -type d ! -name '.'  | exclude_and_chmod u+rwx,go+rx-w
-find . -type f              | exclude_and_chmod u+rw-x,go+r-wx
+find . -type d ! -name '.'    | exclude_and_chmod u+rwx,go+rx-w
+find . -type f                | exclude_and_chmod u+rw-x,go+r-wx
 # find . -type f -name '*.py' | exclude_and_chmod u+rwx,go+rx-w
-find . -type f -name '*.sh' | exclude_and_chmod u+rwx,go+rx-w
+find . -type f -name '*.sh'   | exclude_and_chmod u+rwx,go+rx-w
 
 rm -rf ${temp_dir}
 
